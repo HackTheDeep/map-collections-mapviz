fa6cd9794eb4d2704b2fc2101a767977dea76b73
Beckylee <dr.oreo@gmail.com>
2018-02-11 00:38:27 -0500

    updates style, adds temp & time span to databox

--- source/js/data.js
+++ source/js/data.js
@@ -6,6 +6,10 @@ var data = [{name: 'thing 1', type: 'mammal'},
 {name: 'thing 5', type: 'invertebrate'},
 {name: 'thing 6', type: 'fish'}];
 
+var rangeStart = 1880;
+var rangeEnd = rangeStart + 9;
+var ocean = 70;
+
 var dataBox = document.getElementById('dataBody');
 
 data.forEach(function(elem){
@@ -14,3 +18,13 @@ data.forEach(function(elem){
   newElem.className = elem.type;
   dataBox.appendChild(newElem);
 });
+
+var dataTitle = document.getElementById('dataTitle');
+
+var dateRange = document.createElement('p');
+dateRange.innerHTML = 'Date range: ' + rangeStart + '-' + rangeEnd;
+dataTitle.appendChild(dateRange);
+
+var oceanTemp = document.createElement('p');
+oceanTemp.innerHTML = 'Ave. Ocean Temperature: ' + ocean;
+dataTitle.appendChild(oceanTemp);
